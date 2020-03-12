bfee8fde3b0bdea690152cbe7468bf21ca15c67a
Lappari <joonas.lapinlampi@gmail.com>
2020-03-12 12:27:15 +0200

    fix check cards.ts

--- api/src/util/checkCards.ts
+++ api/src/util/checkCards.ts
@@ -5,10 +5,10 @@ const exec = util.promisify(require('child_process').exec);
 
 export default async function checkCards(pathToImage: String) {
     var cmd :string = '';
-    if (conf.OS = 'LINUX') {
+    if (conf.OS === 'LINUX') {
         cmd = `~/code/clones/darknet-master/darknet detector test ~/code/clones/darknet-master/data/obj.data ~/code/clones/darknet-master/cfg/yolo-obj.cfg ~/code/clones/darknet-master/yolo-obj_3000.weights ${pathToImage}`
-    } else if (conf.OS = 'WONDOWS'){
-        cmd = `darknet.exe detector test obj.data yolo-obj.cfg yolo-obj_3000.weights ${pathToImage}`
+    } else if (conf.OS === 'Windows_NT'){
+        cmd = `darknet.exe detector test src/util/obj.data src/util/yolo-obj.cfg src/util/yolo-obj_4600.weights ${pathToImage} -dont_show`
     } else {
         console.log('Fix your .env file! OS must be LINUX or WINDOWS');
         return;
